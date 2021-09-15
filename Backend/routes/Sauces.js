2b1a8cc19255540bebdd08bf6cac5566fb647347
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-09-15 13:12:36 +0200

    :hammer: Création des routes

---
+++ Backend/routes/Sauces.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controllers/Sauces');
+
+router.get('/', auth, saucesCtrl.getAllStuff);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+
+
+module.exports = router;
