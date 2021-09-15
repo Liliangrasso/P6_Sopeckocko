e9a4dc53840bb88ae30ecce17c410487fd72c89e
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-09-15 13:10:38 +0200

    :hammer: Création du Serveur

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const sauceRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://Lilian:e84757013@cluster0.qnbnd.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Access Header
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
