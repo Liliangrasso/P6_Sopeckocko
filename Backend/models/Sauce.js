7814313409590d105558b0212aeb3e533a0b75c5
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-09-15 13:11:44 +0200

    :hammer: Création du Modèle de données

---
+++ Backend/models/Sauce.js
@@ -0,0 +1,17 @@
+﻿const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    userId: { type: String, required: true},
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    heat: { type: Number, required: false },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number },
+    imageUrl: { type: String, required: false },
+    mainPepper: { type: String, required: false },
+    usersLiked: { type: Array, default: [], required: true },
+    usersDisliked: { type: Array, default: [], required: true }
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
