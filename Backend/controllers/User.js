fa1a430891df839fd9216f319408472903b7487e
Liliangrasso <lilian.grasso.pro@gmail.com>
2021-09-30 14:36:07 +0200

    :hammer: Ajout dotenv et sécurisation de la clé

--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -8,18 +8,21 @@ const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+")
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const regexInject = /[\=\'\'\{\}]/; // ne doit pas contenir les caractères suivants : =, ", ", {, }
 
+const env = require("dotenv").config()
+let userToken = process.env.TOKEN;
+
 exports.signup = (req, res, next) => {
 
     var email = req.body.email;
     var password = req.body.password;
     if (!emailRegex.test(email)) {
-        return res.status(400).json({ 'error': 'email is not valid' });
+        return res.status(401).json({ 'error': 'email is not valid' });
     };
     if (regexInject.test(password)) {
-        return res.status(400).json({ 'error': 'password invalid must no inclue "=}{' });
+        return res.status(401).json({ 'error': 'password invalid must no inclue "=}{' });
     };
     if (!passwordRegex.test(password)) {
-        return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
+        return res.status(401).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
     }
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
@@ -28,7 +31,7 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .then(() => res.status(201).json({ message: 'User created' }))
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
@@ -38,18 +41,18 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                return res.status(401).json({ error: 'User not found !' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                        return res.status(401).json({ error: 'incorrect password !' });
                     }
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
+                            `${userToken}`,
                             { expiresIn: '24h' }
                         )
                     });
